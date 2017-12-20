7fc8a54c0ae6ad7e81ee285e656d1029a068782d
李文静 <Lwj_xyL@163.com>
2017-12-20 17:04:41 +0800

    修改目录

---
+++ UserAnimationPractice/Base/BaseNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseNavigationController.swift
+//  UserAnimationPractice
+//
+//  Created by 李旭阳 on 2017/12/20.
+//  Copyright © 2017年 李文静. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.barTintColor = UIColor.orange
+        self.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
